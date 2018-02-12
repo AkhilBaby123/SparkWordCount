368ad1e1010099fa692052ea55c94c2c4f77fa35
Akhil <akhilbaby00@gmail.com>
2018-02-12 18:10:14 +0530

    Testing branch switch

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -4,7 +4,6 @@ object WordCount {
     println("------- My First SBT program.. Happy Coding -------")
     //val conf = new SparkConf().setMaster("local[1]").setAppName("WordCount")
     println("**** Loaded Spark Configuration *** ")
-    //Test 
-
+    //HIHI
   }
 }
