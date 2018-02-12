5a137ab157175494e46b0b20ba18525f622af85c
Akhil <akhilbaby00@gmail.com>
2018-02-12 18:03:17 +0530

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "SparkWordCount"
+
+version := "0.1"
+
+scalaVersion := "2.10.4"
+
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.0.0"
+
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,10 @@
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    println("------- My First SBT program.. Happy Coding -------")
+    //val conf = new SparkConf().setMaster("local[1]").setAppName("WordCount")
+    println("**** Loaded Spark Configuration *** ")
+
+
+  }
+}
